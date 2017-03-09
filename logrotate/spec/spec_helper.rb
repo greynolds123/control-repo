fd1c5d221ba55383fe22738b6341dcba540ef221
Glynn D. Reynolds <greynolds123@gmail.com>
2017-03-09 08:20:04 -0800

    Updates to Dev

--- logrotate/spec/spec_helper.rb
+++ logrotate/spec/spec_helper.rb
@@ -1,6 +1,2 @@
-require 'rspec-puppet'
-
-RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'modules'))
-  c.manifest_dir = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'manifests'))
-end
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'puppetlabs_spec_helper/module_spec_helper'
