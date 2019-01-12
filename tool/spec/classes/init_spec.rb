286bc6609b49c3766b1e31ed77bc2e92e30f44f5
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-01-12 14:36:33 -0800

    Updated the spec file for the tool mpdule

--- tool/spec/classes/init_spec.rb
+++ tool/spec/classes/init_spec.rb
@@ -5,12 +5,8 @@ require 'spec_helper'
 
  context 'with defaults for all parameters' do
     it { should contain_class( 'tool') }
-  end
-end
-
   context 'with defaults for addiitonal parameters' do
-    let { *.pp } |r| do
-
+    let {('= *.pp') } |r|  do
     it { should contain_class( 'config.r' ) || should contain_class( 'tool::config' ),
      
     it { should contain_class( 'nmap.r' ) || should contain_class( 'tool::nmap' ),
--- tool/spec/classes/nmap_spec.rb
+++ tool/spec/classes/nmap_spec.rb
@@ -2,20 +2,16 @@ require 'spec_helper'
 
 describe('tool::config') do
   let(:facts) {
-    {
-context 'with defaults for all parameters' do
+context 'with defaults for all parameters'  do
     let (:install) {{}}
-    set_encoding: utf-8
+    set_encoding:'utf-8'
+ end
     it do
       it { should_contain_packages('nmap') }
       it { should_contain_ensure('nmap')   }
       it { should_contain_mode('0655')      }
-      it { should_contain_version('latest')}
-       }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+      it { should_contain_version('latest')}.to raise_error(RSpec::Expectations::ExpectationNotMetError)
     end
-  end
- end
-
 latest = Puppet::Util::Execution.execute(
-      "yum" ; "yum -y install nmap",
-    )
+      '"yum" ; "yum -y install nmap"') }
+    end
