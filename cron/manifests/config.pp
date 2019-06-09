39fa1b0cc090c56c944dbaee81e87893a3213e74
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-06-09 09:12:54 -0700

    updates

--- cron/manifests/config.pp
+++ cron/manifests/config.pp
@@ -16,22 +16,3 @@
   minute  => '0',
   }
   }
-
-# This class deploys the cron environment for your environment.
-
-  class cron::config {
-  cron { 'clearCache':
-  ensure  => present,
-  user    => 'root',
-  command => '/bin/sh /root/clearCache2016.sh',
-  hour    => '0',
-  minute  => '15',
-  }
-  cron { 'TuneDatabase':
-  ensure  => present,
-  user    => 'root',
-  command => '/bin/sh /root/TuneDatabase.sh',
-  hour    => '1',
-  minute  => '15',
-  }
-  }
