ac7740b042a2a54a528188c70ef1b1c14976688d
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-06-09 09:19:47 -0700

    updated

--- cron/manifests/mc_cron.pp
+++ cron/manifests/mc_cron.pp
@@ -1,20 +1,11 @@
  #Control  the frequency of how the database is pruned.
-<<<<<<< HEAD
-  class cron::mc_cron { 
-  cron { 'pe-puppet-console-prune-task':
-  ensure   => present,
-  user     => 'root',
-  command  =>'/opt/puppetlabs/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production reports:prune reports:prune:failed upto=${prune_upto} unit=${prune_unit} > /dev/null',
-  hour     => '1',
-  minute   => '0',
-=======
-  class cron::mc_cron {
+  
+ class cron::mc_cron {
   cron { 'pe-puppet-console-prune-task':
   ensure  => 'absent',
   user    => 'root',
   command =>'/opt/puppetlabs/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=$<%= environment %> reports:prune reports:prune:failed "upto=${prune_upto} unit=${prune_unit}" > /dev/null',
   hour    => '0',
   minute  => '15',
->>>>>>> f661b3a03526f113b1823084ffd4808cf261cf70
   }
   }
