775111ede2c59d80acc18a386502feb8e44f2f3b
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-06-09 01:14:34 -0700

    Updated and test code manager

--- gitprod/manifests/remote.pp
+++ gitprod/manifests/remote.pp
@@ -3,20 +3,33 @@
   class gitprod::remote {
   file { '$::envdir':
   ensure  => 'present',
+  }
+  }
 
-  if environmentpath:environment/modules  == prod {
-  require => User['pe-puppet'],
-  require => Group['pe-puppet'],
-  } elsif if $::envdir == dev {
-  require => User['pe-puppet'],
-  require => Group['pe-puppet'],
-  } elsif if $::envdir == Dev_docker {
-  require => User['pe-puppet'],
-  require => Group['pe-puppet'],
-  } elsif if $::envdir == stage {
-  require => User['pe-puppet'],
-  require => Group['pe-puppet'],
-  } else {
-  notice { 'This environment is not managed by this puppetmaster' }
-  }
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
