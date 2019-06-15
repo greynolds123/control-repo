0226927621d24484af1c45370c1317c013a2bd61
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-06-14 17:22:40 -0700

    Testing r10k deploy

--- cron/manifests/config.pp
+++ cron/manifests/config.pp
@@ -7,7 +7,7 @@
   hour    => '0',
   minute  => '15',
   }
- 
+
   cron { 'TuneDatabase':
   ensure  => present,
   user    => 'root',
@@ -15,4 +15,69 @@
   hour    => '1',
   minute  => '0',
   }
+
+  cron { 'R10k':
+  ensure  => present,
+  user    => 'root',
+  command => '/opt/puppetlabs/bin/r10k  deploy environment -p',
+  hour    => '0',
+  minute  => '3',
   }
+
+  cron { 'RsyncDev':
+  ensure  => present,
+  user    => 'root',
+  command => '/bin/rsync -razP /etc/puppetlabs/r10k/Dev/  /etc/puppetlabs/code/environments/dev/modules',
+  hour    => '0',
+  minute  => '10',
+  }
+
+  cron { 'RsyncDev_docker':
+  ensure  => present,
+  user    => 'root',
+  command => '/bin/rsync -razP /etc/puppetlabs/r10k/Dev_docker/  /etc/puppetlabs/code/environments/Dev_docker/modules',
+  hour    => '0',
+  minute  => '10',
+  }
+
+ cron { 'RsyncDevelopment':
+  ensure  => present,
+  user    => 'root',
+  command => '/bin/rsync -razP /etc/puppetlabs/r10k/Development/  /etc/puppetlabs/code/environments/Development/modules',
+  hour    => '0',
+  minute  => '10',
+  }
+
+   cron { 'RsyncLinux_Server':
+   ensure  => present,
+   user    => 'root',
+   command => '/bin/rsync -razP /etc/puppetlabs/r10k/Linux_Server/  /etc/puppetlabs/code/environments/Linux_Server/modules',
+  hour     => '0',
+  minute   => '10',
+  }
+
+ cron { 'Rsyncmaster':
+  ensure  => present,
+  user    => 'root',
+  command => '/bin/rsync -razP /etc/puppetlabs/r10k/master/  /etc/puppetlabs/code/environments/master',
+  hour    => '0',
+  minute  => '10',
+  }
+
+ cron { 'Rsyncproduction':
+  ensure  => present,
+  user    => 'root',
+  command => '/bin/rsync -razP /etc/puppetlabs/r10k/production/  /etc/puppetlabs/code/environments/production/modules',
+  hour    => '0',
+  minute  => '10',
+  }
+
+ cron { 'Rsyncstage':
+  ensure  => present,
+  user    => 'root',
+  command => '/bin/rsync -razP /etc/puppetlabs/r10k/stage/  /etc/puppetlabs/code/environments/stage/modules',
+  hour    => '0',
+  minute  => '10',
+  }
+  }
+
