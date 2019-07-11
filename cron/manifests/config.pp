7c13a875bffad4ba859780fc3f50e912738162f0
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-07-11 12:49:30 -0700

    Updated production module

--- cron/manifests/config.pp
+++ cron/manifests/config.pp
@@ -5,66 +5,15 @@
   ensure  => present,
   user    => 'root',
   command => '/bin/sh /root/clearCache2016.sh',
-  hour    => '0',
-  minute  => '15',
+  hour    => '1',
+  minute  => '45',
   }
  
   cron { 'TuneDatabase':
   ensure  => present,
   user    => 'root',
   command => '/bin/sh /root/TuneDatabase.sh',
-  hour    => '1',
-  minute  => '0',
-  }
-  }
-  
-  cron { 'R10k':
-  ensure  => absent,
-  user    => 'root',
-  command => '/opt/puppetlabs/bin/r10k  deploy environment -p',
   hour    => '0',
-  minute  => '3',
+  minute  => '45',
   }
-
-  cron { 'RsyncDev':
-  ensure  => absent,
-  user    => 'root',
-  command => '/bin/rsync -razP /etc/puppetlabs/code/modules/puppet_Dev/*  /etc/puppetlabs/code/environments/dev/modules',
-  hour    => '0',
-  minute  => '10',
   }
-
-  cron { 'RsyncDev_docker':
-  ensure  => absent,
-  user    => 'root',
-  command => '/bin/rsync -razP /etc/puppetlabs/code/modules/puppet_Dev_docker/*  /etc/puppetlabs/code/environments/Dev_docker/modules',
-  hour    => '0',
-  minute  => '10',
-  }
-
-
-  cron { 'Rsyncproduction':
-  ensure  => absent,
-  user    => 'root',
-  command => '/bin/rsync -razP /etc/puppetlabs/code/modules/puppet_production/*  /etc/puppetlabs/code/environments/production/modules',
-  hour    => '0',
-  minute  => '10',
-  }
-
-  cron { 'Rsyncmaster':
-  ensure  => absent,
-  user    => 'root',
-  command => '/bin/rsync -razP /etc/puppetlabs/code/modules/puppet_master/*  /etc/puppetlabs/code/environments/master',
-  hour    => '0',
-  minute  => '10',
-  }
-
-
-  cron { 'Rsyncstage':
-  ensure  => absent,
-  user    => 'root',
-  command => '/bin/rsync -razP /etc/puppetlabs/code/modules/puppet_stage/*  /etc/puppetlabs/code/environments/stage/modules',
-  hour    => '0',
-  minute  => '10',
-  }
-
--- sudo/manifests/service.pp
+++ sudo/manifests/service.pp
@@ -6,8 +6,4 @@ define sudo::service() {
     source  => "puppet:///modules/sudo/common/etc/sudoers.d/${name}",
     require => Package['sudo'],
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1de4402b3b517d4a5ec3b988913cd26786d0111c
