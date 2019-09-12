0810b7a2b4536016a642f54573b3b82e2ebea1ba
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-09-11 23:01:19 -0700

    Updated function

---
+++ tool/lib/function/config_function.rb
@@ -0,0 +1,32 @@
+Puppet::Function.create_function('tool::config') do
+
+  dispatch :config do
+
+    parma 'String', :filename
+
+    return_type 'String'
+
+   end
+
+
+   def config(filename)
+
+     File.config(filename)
+
+   end
+
+
+# This sections links the function to the module class.
+
+   function tool::config(Variant[String, Boolean] $arg) >> String {
+
+     case $arg {
+
+     false, undef, /(?i::false)/ : { '0'  }
+
+     true, /(?i:true)/           : { '1'   }
+
+     default                     : { "$arg" }
+
+    }
+   }
