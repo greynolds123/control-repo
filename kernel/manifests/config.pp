37e1dbb636426b218ffc573162d1c390e3a69005
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2019-11-13 00:44:56 -0800

    removed old stdlib

--- kernel/manifests/config.pp
+++ kernel/manifests/config.pp
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6fcbf65f5da55a17e187ee1a6fd36bbb7963e538
 # This class is the configuration file for kernel updates.
 
   class kernel::config (
@@ -27,36 +23,4 @@
   allow_virtual => $allow_virtual,
   }
   }
-<<<<<<< HEAD
-    
-=======
-# This class is the configuration file for kernel updates.
 
-  class kernel::config (
-  $ensure     = 'present',
-  $allow_virtual = false,
-  ) {
-
-
-  yumrepo    { 'Redhat_7_Updates':
-  ensure   => 'present',
-  baseurl  => 'http://centos7.prod.localdomain/yum/channels/$baseos/rhel-x86_64-server-7',
-  descr    => 'RHEL7 udpates channel',
-  enabled  => '1',
-  gpgcheck => '0',
-  gpgkey   => 'http://centos7.prod.localdomain/yum/keys/RPM-GPG-KEY-redhat-release
-  http://centos7.prod.localdomain/yum/keys/RPM-GPG-KEY-redhat-release7',
-  priority => '1',
-  }
-
-  package { 'kernel-3.10.0-229.1.2.el7':
-  ensure        => $ensure,
-  provider      => 'yum',
-  allow_virtual => $allow_virtual,
-  }
-  }
-    
->>>>>>> c85f3f0a34a6f9d54ee4608b5f8d792152f48899
-=======
-  
->>>>>>> 6fcbf65f5da55a17e187ee1a6fd36bbb7963e538
